f4fda074bd4323b6d2c923d28b28d0fb7d41c517
Matthew OBzera <mobzera516@gmail.com>
2017-05-01 22:59:00 +0000

    Scaffold

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'login_session/new'
+
+  resources :roster_students
+  resources :roster_squares
+  resources :session_events
+  resources :sessions
+  resources :squares
+  resources :students
+  resources :teachers
+  resources :schools
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
