3dbbdc56d81b86b869bec9bf24328aef9bcd9c5b
Matthew OBzera <mobzera516@gmail.com>
2017-05-03 21:33:51 +0000

    Setup relationships

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,7 @@
 class Teacher < ApplicationRecord
+    has_many :active_relationships, class_name:  "RosterStudent",
+                                    foreign_key: "teacher_id",
+                                    dependent:   :destroy
+    
+    has_many :students, through: :active_relationships, source: :student
 end
