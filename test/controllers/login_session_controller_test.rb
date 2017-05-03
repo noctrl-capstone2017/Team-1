a1e3ebc77d1866b50767d68a312ab07ff2010a93
Michael Loptien <mloptien@noctrl.edu>
2017-05-03 23:10:41 +0000

    Formatting Student Pages and adding validation

--- test/controllers/login_session_controller_test.rb
+++ test/controllers/login_session_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class LoginSessionControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get login_session_new_url
-    assert_response :success
-  end
 
 end
