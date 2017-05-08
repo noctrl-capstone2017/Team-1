171a1888bfd772dde6674db70b5e2aad255e101c
Michael Loptien <mloptien@noctrl.edu>
2017-05-08 03:08:53 +0000

    Adding Square views 1, 2, & 3

--- app/models/square.rb
+++ app/models/square.rb
@@ -1,2 +1,9 @@
+# Michael Loptien
+
 class Square < ApplicationRecord
+  # Ensures that none of the fields are empty
+  validates :square_id, presence: true
+  validates :square_type, presence: true
+  validates :color, presence: true
+  validates :square_description, presence: true
 end
